636b820e87df5de7df21b00dc68951951a4d6bdc
Tutu <why_joseph@outlook.com>
2024-10-15 15:35:06 +0800

    Complete order list development

--- miniprogram/modules/orderPayModule/pages/order/list/list.js
+++ miniprogram/modules/orderPayModule/pages/order/list/list.js
@@ -8,7 +8,8 @@ Page({
         orderList: [1, 2, 3],
         page: 1,
         limit: 10,
-        total: 0
+        total: 0,
+        isLoading: false
     },
 
     // Get order list
@@ -18,19 +19,51 @@ Page({
             limit
         } = this.data
 
+        this.isLoading = true
+
         // Call api function to get order list
         const res = await reqOrderList(page, limit)
-        console.log(res)
+        // console.log(res)
 
-        if(res.code === 200) {
+        this.isLoading = false
+
+        if (res.code === 200) {
             this.setData({
-                orderList: res.data.records,
+                orderList: [...this.data.orderList, ...res.data.records],
                 total: res.data.total
             })
         }
     },
 
-    onLoad () {
+    // When reaching the bottom of current page
+    onReachBottom() {
+        const {
+            page,
+            total,
+            orderList,
+            isLoading
+        } = this.data
+
+        // if loading not compelte, do not load next page
+        if (isLoading) return
+
+        // Compare "Total number of data" and "Order list length"
+        if (total === orderList.length) {
+            return wx.showToast({
+                title: '数据加载完毕'
+            })
+        }
+
+        // Update page
+        this.setData({
+            page: page + 1
+        })
+
+        // Resend request
+        this.getOrderList()
+    },
+
+    onLoad() {
         this.getOrderList()
     }
 
